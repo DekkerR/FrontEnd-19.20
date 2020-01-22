bfec562fe2e59e5002df0668252b7770e9f85689
DekkerR <Rickdekkerr@hotmail.com>
2020-01-22 09:46:49 +0100

    Add files via upload

---
+++ week3/js/script.js
@@ -0,0 +1,11 @@
+var testbutton = document.querySelector('input');
+var zoekveld= document.querySelector('header');
+
+object.onClick = function() {myScript};
+
+object.addEventListener("click", myScript);
+
+
+function testButton() {
+	
+}
